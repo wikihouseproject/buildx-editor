944259bf539a93e2c5ff89fe166980970a3f000f
Jon Nordby <jononor@gmail.com>
2017-07-18 17:29:29 +0200

    export: Prep moving SVG export to wren

--- src/components/svg.js
+++ src/components/svg.js
@@ -5,17 +5,29 @@ import {intent, model, renderControls} from '../extras/functions'
 
 const SVG = require('../lib/wren/patterns/svg.js')
 
-const cutPaths = (width, height, wallHeight) => {
-  const out = wren.frame({width: width*100, height: height*100, wallHeight: wallHeight*100, frameWidth: 10});
-
+const frameCuts = (frame) => {
   // TODO: include naming and grouping
   var paths = [];
   for (var i=0; i<5; i++) {
-    const p = out.close(out.points(i));
+    const p = SVG.closedPath(frame.points(i));
     paths.push(p);
   }
-
   return paths;
+}
+
+// Render as a SVG vdom tree
+function renderSvg(geometry, options) {
+  options = options || {};
+  options.width = options.width || 600;
+  options.height = options.height || 600; 
+
+  var cuts = frameCuts(geometry.frame).map( (p) =>
+    h('path', {attrs: { fill: 'none', stroke: 'red', d: p }})
+  )
+  const b = h('path', { attrs: { fill: 'none', stroke: 'grey', d: SVG.closedPath(geometry.boundary) }});
+  const paths = cuts.concat([b]);
+
+  return h('svg', {attrs:{ xmlns: 'http://www.w3.org/2000/svg', width: options.width, height: options.height }}, paths);
 }
 
 // SVG conventions
@@ -39,17 +51,16 @@ export default function renderCutsheets(sources) {
 
   const vtree$ = state$.map(([width, height, wallHeight, length]) => {
 
-    var cuts = cutPaths(width, height, wallHeight).map( (p) =>
-      h('path', {attrs: { fill: 'none', stroke: 'red', d: p }})
-    )
+    const workarea = { width: 1.2, height: 2.4 }; // TODO: pass as parameters
+    const cutsheet = rectangle(workarea.width*100, workarea.height*100);
 
-    const cutsheet = { width: 1.2, height: 2.4 }; // TODO: pass as parameters
-    const r = rectangle(cutsheet.width*100, cutsheet.height*100);
-    const c = h('path', { attrs: { fill: 'none', stroke: 'grey', d: SVG.closedPath(r) }});
-    const paths = cuts.concat([c]);
+    const geometry = {
+      'frame': wren.frame({width: width*100, height: height*100, wallHeight: wallHeight*100, frameWidth: 10}),
+      'boundary': cutsheet
+    };
 
     return div([
-      h('svg', {attrs:{ xmlns: 'http://www.w3.org/2000/svg', width: 600, height: 600 }}, paths),
+      renderSvg(geometry),
       ...renderControls(width, height, wallHeight, length)
     ])
   })
