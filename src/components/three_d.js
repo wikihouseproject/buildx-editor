1bf6b40347f791a28e8e15735f930b6022020e5b
John Rees <john@bitsushi.com>
2017-07-18 15:37:08 +0100

    orbit controls camera

--- src/components/three_d.js
+++ src/components/three_d.js
@@ -1,5 +1,6 @@
 import '../lib/aframe-components/aframe-extrude-svg-component'
 import '../lib/aframe-components/aframe-clone-component'
+import 'aframe-orbit-controls-component-2'
 
 import {h, div} from '@cycle/dom'
 import xs from 'xstream'
@@ -43,6 +44,9 @@ export default function ThreeD(sources) {
       h('a-scene', {attrs: {stats: true}}, [
 
         h('a-entity', {attrs: {id: 'frames', position: '-2.4 6 -10', rotation: '180 0 0'}}, [
+
+          h('a-entity', {attrs:{ id: 'cameraTarget', position: '0 5 0' }}),
+
           h('a-entity', {attrs: {id: 'frame', position: '0 0 0'}}, [
             piece([width, height, wallHeight], 0, 'yellow'),
             piece([width, height, wallHeight], 1, 'green'),
@@ -58,6 +62,23 @@ export default function ThreeD(sources) {
           geometry:{ primitive: 'plane', width: 20, height: 20},
           material: {color: '#CCC', side: 'double'},
           position: '0 -0.01 -10', rotation: '-90 0 0'
+        }}),
+
+
+        h('a-entity', {attrs:{
+          id: 'camera',
+          camera: { fov: 90, zoom: 1 },
+          position: {x: 0, y: 5, z: 4 },
+          'orbit-controls': {
+            target: '#cameraTarget',
+            autoRotate: false,
+            enableDamping: true,
+            dampingFactor: 0.125,
+            rotateSpeed:0.1,
+            minDistance:3,
+            maxDistance:100,
+            maxPolarAngle: Math.PI/2
+          }
         }})
 
       ]),
