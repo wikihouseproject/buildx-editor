c5959e085a57c90c5be867394c7ebc9adf58a50b
John Rees <john@bitsushi.com>
2017-07-18 17:37:58 +0100

    use setObject3D and removeObject3D instead of accessing object3D directly

--- src/lib/aframe-components/aframe-arrow-helper-component.js
+++ src/lib/aframe-components/aframe-arrow-helper-component.js
@@ -1,29 +1,27 @@
 AFRAME.registerComponent('arrow-helper', {
   schema: {
     dir: {default: {x: 0, y: 1, z: 0}, type: 'vec3'}, // direction from origin. Must be a unit vector.
-    // origin: {type: 'vec3'}, // Point at which the arrow starts.
+    // origin: {type: 'vec3'}, // Point at which the arrow starts. (use position instead)
     length: {default: 1, type: 'number'}, // length of the arrow
-    color: {default: '#ffff00', type: 'color'},
+    color: {default: '#ffff00', type: 'color'}, // (called HEX in Three.js)
     headLength: {type: 'number'}, // Default is 0.2 * length.
     headWidth: {type: 'number'} // Default is 0.2 * headLength.
   },
   init: function () {
     const {dir, origin, length, color} = this.data;
-    const {object3D} = this.el;
-    const normalizedDir = new THREE.Vector3(...Object.values(dir)).normalize()
 
-    object3D.add(
-      new THREE.ArrowHelper(
-        normalizedDir,
-        object3D.position,
-        length,
-        color
-      )
+    const normalizedDir = new THREE.Vector3(...Object.values(dir)).normalize()
+    const arrowHelper = new THREE.ArrowHelper(
+      normalizedDir,
+      this.el.object3D.position,
+      length,
+      color
     )
+    this.el.setObject3D('arrowHelper', arrowHelper)
   },
   // update: function () { console.log('update') },
-  // remove: function () {
-  //   this.el.removeObject3D('mesh')
-  // }
+  remove: function () {
+    this.el.removeObject3D('arrowHelper')
+  }
 
 });
