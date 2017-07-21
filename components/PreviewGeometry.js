0ae102757195ffba2a5985a60dae53a06e98f86f
Jon Nordby <jononor@gmail.com>
2017-07-21 16:18:44 +0200

    Support previewing 2d point arrays

--- components/PreviewGeometry.js
+++ components/PreviewGeometry.js
@@ -1,8 +1,12 @@
 const noflo = require('noflo');
 
+//import * as components from '../src/components'; // XXX: using import makes 'exports' go away
+
+const components = require('../src/components');
+
 exports.getComponent = function() {
   var c = new noflo.Component();
-  c.description = 'Get default Wren model parameters';
+  c.description = 'Render geometry into 3d-view';
   c.icon = 'forward';
   c.inPorts.add('in', {
     datatype: 'all',
@@ -21,12 +25,31 @@ exports.getComponent = function() {
 
     const scene = window.scene; // XXX: hack
 
-    var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-    var material = new THREE.MeshBasicMaterial( { color: 0xff0000 } );
-    var cube = new THREE.Mesh( geometry, material );
-    scene.add(cube)
+    const is2dPoint = (p) => {
+      return p && Array.isArray(p) && Array.isArray(p.length == 2)
+    }
+    const is2dPointArray = (a) => {
+      const nonEmptyArray = a && Array.isArray(a) && a.length;
+      const childIs2d = is2dPoint(a[0]);
+      console.log('a', a, nonEmptyArray, childIs2d);
+      return nonEmptyArray && childIs2d;
+    }
 
-    const out = cube;
+    var items = [];
+    if (geometry == 'box') {
+      var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+      var material = new THREE.MeshBasicMaterial( { color: 0xff0000 } );
+      var cube = new THREE.Mesh( geometry, material );
+      items.push(cube);
+    } else if (is2dPointArray(geometry)) {
+      const outline = components.outline(geometry, 10);
+      items.push(outline);
+    }
+
+    items.map((i) => {
+      scene.add(i);
+    });
+    const out = items;
 
     // Process data and send output
     output.send({
