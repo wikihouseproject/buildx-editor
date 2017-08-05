3d49d991d44a30a75017db0e51fe05b694518df7
John Rees <john@bitsushi.com>
2017-08-05 21:41:06 +0100

    removed roof files

--- src/lib/wren/__tests__/outputs/pieces/side.js
+++ src/lib/wren/__tests__/outputs/pieces/side.js
@@ -5,10 +5,9 @@ it('generates a side', () => {
     height: 200,
     width: 100
   }
-  expect(side(params)).toEqual([
-    [0,200],
-    [100,200],
-    [100,0],
-    [0,0]
-  ])
+  expect(side(params)).toEqual({
+    pts: [[0, 200], [100, 200], [100, 0], [0, 0]],
+    pos: {x: 0, y: 0, z: 0},
+    rot: {x: 0, y: 0, z: 0}
+  })
 })
--- src/lib/wren/outputs/pieces/side.js
+++ src/lib/wren/outputs/pieces/side.js
@@ -4,13 +4,33 @@
  * @param {Number} width
  * @return {Array} side
  */
+
+// s = finShape(params)
+// roofComponent(s, params, s.inner, s.sides.rightRoof, {y: params.height - s.inner[0][1]/100 + params.frameWidth/2 }),
+// const roofComponent = (params, arr, [from,to], position={}, rotation={}) => {
+//   const roofLength = Math.min(math.distance(arr[from], arr[to]), params.sheetLength*100)
+//   const roofAngle = Math.atan2(params.width/2, (params.height-params.wallHeight))
+//   const rotation = { z: Math.PI/2, x: -Math.PI/2, y: roofAngle-Math.PI/2 }
+//   const points = [
+//     [0,roofLength],
+//     [params.bayLength*100,roofLength],
+//     [params.bayLength*100,0],
+//     [0,0]
+//   ]
+//   return [points, position, rotation]
+// }
+
+
 const side = ({height, width}) => {
-  return [
+  const pos = { x: 0, y: 0, z: 0 }
+  const rot = { x: 0, y: 0, z: 0 }
+  const pts = [
     [0, height],
     [width, height],
     [width, 0],
     [0, 0]
   ]
+  return {pts, pos, rot}
 }
 
 module.exports = side
