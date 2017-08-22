2be1e26423ae077f7551b6e7d99962422e28e658
Jon Nordby <jononor@gmail.com>
2017-08-22 20:49:48 +0200

    Wren: Fix frame positioning
    Makes it fit within sheets of the bays
    
    Note: Right now bay sheeting does not cover the sides of last frame

--- src/lib/wren/outputs/pieces/frame/index.js
+++ src/lib/wren/outputs/pieces/frame/index.js
@@ -3,13 +3,14 @@ const fin = require('./fin')
 const Frame = (points, inputs, index=0) => {
 
   const bayLength = inputs.dimensions.bayLength/1000
-  const frameZ = (bayLength * (index-1)) - (bayLength * inputs.dimensions.bays)/2 - (inputs.dimensions.finDepth/1000)/2
-  const frameX = inputs.dimensions.width/1000
+  const frameZ = (bayLength * (index-1)) - (bayLength * inputs.dimensions.bays)/2
+  const frameX = -(inputs.dimensions.width/1000 + inputs.dimensions.finDepth/1000)/2
+  const frameY = (inputs.dimensions.roofApexHeight/1000) + inputs.dimensions.beamWidth/1000
 
   const finsGroup = fin(points).map(piece => ({
     pts: piece.map( ([x,y]) => ([x/1000,y/1000]) ),
-    pos: { x: frameX, y: 0, z: frameZ },
-    rot: { x: 0, y: 0, z: 0, order: 'XYZ' }
+    pos: { x: frameX, y: frameY, z: frameZ },
+    rot: { x: Math.PI, y: 0, z: 0, order: 'XYZ' }
   }))
 
   return {
