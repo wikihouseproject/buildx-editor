13562d711fb888fd287683d792772b29e82ab12e
Jon Nordby <jononor@gmail.com>
2017-08-25 01:29:52 +0200

    Log execution time in wren

--- src/lib/wren/index.js
+++ src/lib/wren/index.js
@@ -4,7 +4,9 @@ const _outputs = require('./outputs')
 
 function Wren(overrides) {
   const inputs = merge(defaults, overrides)
+  console.time('wren')
   const outputs = _outputs(inputs)
+  console.timeEnd('wren')
 
   const toSVG = (options) => outputs.formats.svg(options)
 
