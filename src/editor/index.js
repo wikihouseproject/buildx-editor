68a3e7a2663dab9e427e3cc208348362378bcc62
Jon Nordby <jononor@gmail.com>
2017-08-28 00:50:49 +0200

    editor: Integrate Noflo live mode

--- src/editor/index.js
+++ src/editor/index.js
@@ -7,6 +7,9 @@ import House from './components/house'
 import { merge } from "lodash"
 import Wren from "../lib/wren"
 
+import * as uuid from 'uuid';
+import * as nofloTools from '../lib/fbptools';
+
 // Export so NoFlo build can use it
 window.wren = Wren
 
@@ -25,6 +28,12 @@ var wrenWorker = (USING_WEBWORKERS) ? new WrenWorker : null
 let dimensions = Wren.inputs({}).dimensions
 const changeDimensions = house => newDimensions => {
   dimensions = merge(dimensions, newDimensions)
+
+  if (nofloNetworkLive) {
+    sendToRuntime(nofloRuntime, lastGraphName, 'parameters', { dimensions })
+    return
+  }
+
   if (USING_WEBWORKERS) {
     wrenWorker.postMessage({dimensions})
   } else {
@@ -63,6 +72,7 @@ loader.load('img/materials/plywood/birch.jpg',
 )
 
 function prerender() {
+
   Wren({dimensions}).then((res) => {
 
   const house = House(res.outputs.pieces)
@@ -70,6 +80,10 @@ function prerender() {
   if (USING_WEBWORKERS) {
     wrenWorker.onmessage = event => house.update(event.data.pieces)
   }
+
+  setupRuntime((updatedGeometry) => {
+    house.update(updatedGeometry.pieces)
+  })
 
   const hud = HUD(dimensions, changeDimensions(house))
 
@@ -90,4 +104,111 @@ function render() {
   requestAnimationFrame(render)
 }
 
-// requestAnimationFrame(render)
+// NoFlo runtime setup
+var nofloRuntime = null;
+var lastGraphName = 'default/main';
+var nofloNetworkLive = false
+
+function setupRuntime(onOutput) {
+
+  setupNoFlo((err, runtime) => {
+    if (err) {
+      throw err;
+    }
+    nofloRuntime = runtime;
+
+    // Allow swithi
+    const url = nofloTools.flowhubURL(runtime.id);
+    const link = flowhubLink(url);
+    link.id = 'flowhubLink';
+    link.addEventListener('click', function (event) {
+      event.preventDefault();
+      event.stopPropagation();
+
+      connectRuntime(nofloRuntime, onOutput) // switch to using NoFlo network for geometry calc
+      runtime.openClient(link.getAttribute('href')); // Open Flowhub
+    });
+    document.body.appendChild(link);
+  })
+
+}
+
+function connectRuntime(runtime, onOutput) {
+  nofloNetworkLive = true
+
+  const sendInputs = () => {
+    sendToRuntime(runtime, lastGraphName, 'parameters', { dimensions } )
+  }
+
+  runtime.runtime.on('ports', (ports) => {
+    lastGraphName = ports.graph
+    // Assume a different network, and inputs need to be sent anew
+    sendInputs()
+  })
+
+  // Data sent by NoFlo network
+  runtime.runtime.on('packet', (msg) => {
+      if (msg.event != 'data') {
+          // ignore connect/disconnect
+          return
+      }
+      //console.log('received', msg.port, msg)
+      if (msg.port == 'geometry') {
+        setTimeout(() =>
+          onOutput(msg.payload)
+        , 0)
+      } else {
+        console.log('NoFlo sent data on unknown port:', msg)
+      }
+  })
+
+  // Send current values
+  sendInputs()
+}
+
+function sendToRuntime(runtime, graphName, port, data) {
+  //console.log('sending', graphName, port, data)
+  const packet = {
+    event: 'data',
+    graph: graphName,
+    port,
+    payload: data,
+  }
+  runtime.runtime.sendPacket(packet, (err) => {
+    if (err) {
+      console.error('send packet failed', err)
+    }
+  })
+}
+
+function setupNoFlo(callback) {
+
+  // Use a persistent runtime id
+  const idKey = 'noflo_runtime_id';
+  const storage = window.localStorage; // could also use sessionStorage
+  var runtimeId = storage.getItem(idKey);
+  if (!runtimeId) {
+    runtimeId = uuid.v4();
+    storage.setItem(idKey, runtimeId);
+  }
+
+  const o = {
+    id: runtimeId,
+    namespace: 'buildx-editor',
+    repository: 'wikihouse-project/buildx-editor',
+  }
+
+  nofloTools.setupAndRun(o, (err, runtime) => {
+    return callback(err, runtime);
+  });
+
+}
+
+function flowhubLink(url) {
+  var link = document.createElement('a');
+  link.innerHTML = "Open in Flowhub";
+  link.className = "debug";
+  link.href = url;
+  link.target = '_blank';
+  return link;
+}
