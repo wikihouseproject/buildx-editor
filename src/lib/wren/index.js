3b64a56533f2922e5f64ef01943336a7c2ed9747
Jon Nordby <jononor@gmail.com>
2017-08-27 17:07:53 +0200

    Revert "Log execution time in wren"
    This reverts commit 13562d711fb888fd287683d792772b29e82ab12e.

--- src/lib/wren/index.js
+++ src/lib/wren/index.js
@@ -4,9 +4,7 @@ const _outputs = require('./outputs')
 
 function Wren(overrides) {
   const inputs = merge(defaults, overrides)
-  console.time('wren')
   const outputs = _outputs(inputs)
-  console.timeEnd('wren')
 
   const toSVG = (options) => outputs.formats.svg(options)
 
